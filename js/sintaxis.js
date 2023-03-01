09dd7f9a9b6d094f367deee5832ec01562513910
Johanna Soledad Chavez Ayala <joha_joys@hotmail.com>
2023-02-28 22:58:46 -0300

    dar formato a doc

--- js/sintaxis.js
+++ js/sintaxis.js
@@ -1,15 +1,14 @@
-function mostrarOcultar(idElemento, idBoton){
-   
-    
-if (document.getElementById(idBoton).innerHTML == 'Más sobre mí') {
+function mostrarOcultar(idElemento, idBoton) {
+
+
+  if (document.getElementById(idBoton).innerHTML == 'Más sobre mí') {
     document.getElementById(idElemento).style.display = 'block';
     document.getElementById(idBoton).innerHTML = "mostrar menos";
-  } 
+  }
   else {
     document.getElementById(idElemento).style.display = 'none';
     document.getElementById(idBoton).innerHTML = "Más sobre mí";
   }
-    }
+}
 
 
-   
